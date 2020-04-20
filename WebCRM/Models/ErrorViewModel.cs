5b8ef149c98dbe6b9363f62eb94223e8fab01aea
Hieu Vo <voviethieu198@gmail.com>
2020-04-20 20:44:42 +0700

    Add project files.

---
+++ WebCRM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebCRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
